9fac1e2a604d264de1cf8b1652e9b883a0645a69
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2023-12-28 12:36:17 +1100

    Add Campaign struct and mapping

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -3,4 +3,20 @@ pragma solidity ^0.8.9;
 
 contract MyContract {
     constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 goal;
+        uint256 deadline;
+        uint256 amountRaised;
+        string image;
+        address[] contributors;
+        uint256[] contributions;
+
+    }
+    mapping(address => Campaign) campaigns;
+    uint256 public numberOfCampaigns = 0;
+    
+
 }

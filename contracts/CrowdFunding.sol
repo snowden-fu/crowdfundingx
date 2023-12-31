88aa635aeeaf754d92711682e805e0551c4a4908
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-01 09:11:07 +1100

    Refactor CrowdFunding contract and add new functions

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
+contract CrowdFunding {
     constructor() {}
     struct Campaign {
         address owner;
@@ -15,6 +15,8 @@ contract MyContract {
         uint256[] contributions;
 
     }
+
+    // create a mapping of campaignId to Campaign, initialize to 0
     mapping(uint256 => Campaign) campaigns;
     uint256 public numberOfCampaigns = 0;
 
@@ -32,5 +34,43 @@ contract MyContract {
 
         numberOfCampaigns++;
         return numberOfCampaigns-1;
+    }
+
+    function getCampaign(uint256 _campaignId) public view returns (address, string memory, string memory, uint256, uint256, uint256, string memory, address[] memory, uint256[] memory){
+        Campaign storage campaign = campaigns[_campaignId];
+        return (campaign.owner, campaign.title, campaign.description, campaign.goal, campaign.amountRaised, campaign.deadline, campaign.image, campaign.contributors, campaign.contributions);
+    }
+
+    function contribute(uint256 _campaignId) public payable{
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_campaignId];
+        campaign.contributors.push(msg.sender);
+        campaign.contributions.push(amount);
+
+        (bool success, ) = campaign.owner.call{value: amount}("");
+
+        if(success){
+            campaign.amountRaised += amount;
+        }
+        else{
+            revert("Transfer failed");
+        }
+
+    }
+    
+    function getContributors(uint256 _campaignId) public view returns (address[] memory){
+        return campaigns[_campaignId].contributors;
+    }
+
+    function getContributions(uint256 _campaignId) public view returns (uint256[] memory){
+        return campaigns[_campaignId].contributions;
+    }
+
+    function getAllCampaigns() public view returns (Campaign[] memory){
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for(uint256 i = 0; i < numberOfCampaigns; i++){
+            allCampaigns[i] = campaigns[i];
+        }
+        return allCampaigns;
     }
 }

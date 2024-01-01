beede20415d5ae964458bcd93ef6fb3cbdc56a90
Snowden Fu <24203166+snowden-fu@users.noreply.github.com>
2024-01-02 10:04:34 +1100

    Add Sepolia network configuration to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,6 +33,13 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia",
+        accounts: `0x${process.env.PRIVATE_KEY}`,
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
